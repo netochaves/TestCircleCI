a5c89e513087e5ce05e0ec97001bfca3d47db601
Neto Chaves <netobac1@gmail.com>
2019-04-27 19:14:54 -0300

    lint ci test

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from "react"
 import { Platform, StyleSheet, Text, View } from "react-native"
 
@@ -17,6 +9,7 @@ const instructions = Platform.select({
 })
 
 export default class App extends Component {
+  state = {}
   render() {
     return (
       <View style={styles.container}>
