812b125b4f3e9e6ad9f7c173825dd679bcee25fc
Neto Chaves <netobac1@gmail.com>
2019-04-27 18:27:54 -0300

    pull request test

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native Neto Chaves!</Text>
+        <Text style={styles.welcome}>This is a pull request ci test</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
